584bd54da14dd00762ed1872d7e768b64c01d354
Allan <allanmelo979@gmail.com>
2026-04-07 17:29:21 -0300

    correção. lan

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+//import App from './App.tsx' IMPORTANTE!!!: não deixar NENHUMA variável ou função sem uso, pois o Vite não compila o projeto, e não é possível rodar o projeto. Se for necessário, comentar a linha de código, ou usar a variável ou função.
 import './index.css'
 import LandingPage from './pages/padrao/landing-page'
 
