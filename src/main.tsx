509557c3b96230e4f90edd68b7ef2aa7fc01e4a5
Allan <allanmelo979@gmail.com>
2026-04-07 17:59:03 -0300

    Teste de routes. lan

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.tsx' IMPORTANTE!!!: não deixar NENHUMA variável ou função sem uso, pois o Vite não compila o projeto, e não é possível rodar o projeto. Se for necessário, comentar a linha de código, ou usar a variável ou função.
+import App from './App.tsx' 
 import './index.css'
-import LandingPage from './pages/padrao/landing-page'
+
+//IMPORTANTE!!!: não deixar NENHUMA variável ou função sem uso, pois o Vite não compila o projeto, 
+// e não é possível rodar o projeto. Se for necessário, comentar a linha de código, ou usar a variável 
+// ou função.
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LandingPage />
+    <App />
   </StrictMode>,
 )
